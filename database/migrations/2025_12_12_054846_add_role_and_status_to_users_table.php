aa03780445ac91179ceaefa6c79c4655a24eb00c
Alexis Andrade <alexis.andrade@gmail.com>
2025-12-12 00:01:08 -0600

    Define role and is_active fields on users table

--- database/migrations/2025_12_12_054846_add_role_and_status_to_users_table.php
+++ database/migrations/2025_12_12_054846_add_role_and_status_to_users_table.php
@@ -9,20 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+ public function up(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        // admin, empleado, cliente
+        $table->string('role', 20)->default('cliente')->after('password');
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+        // 1 = activo, 0 = desactivado
+        $table->boolean('is_active')->default(true)->after('role');
+    });
+}
+
+public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn(['role', 'is_active']);
+    });
+}
+
 };
