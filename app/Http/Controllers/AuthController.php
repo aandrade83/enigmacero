fdd30933ef96c53ce716e99a37c211b044951726
Alexis Andrade <alexis.andrade@gmail.com>
2025-12-13 15:27:53 -0600

    Fix Session facade import in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session; 
 
 
 class AuthController extends Controller
