c8e2599d1880d74e71fa358f8864e524b93913fd
Alexis Andrade <alexis.andrade@gmail.com>
2025-12-16 11:46:48 -0600

    Modify the client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'bucket_folder',
+        'internal_email',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
--- database/migrations/2025_12_16_172035_create_clients_table.php
+++ database/migrations/2025_12_16_172035_create_clients_table.php
@@ -9,13 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('clients', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  public function up(): void
+{
+    Schema::create('clients', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('bucket_folder')->unique();     // prefijo/pseudo-carpeta en GCS
+        $table->string('internal_email')->nullable();  // correo interno
+        $table->boolean('is_active')->default(true);
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
